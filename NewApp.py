54ddb3e0ef5fe5c4d50c1a610ee54cf84ea1b658
anayek@newamps.co.in <CodeLmp?006>
2019-12-26 18:43:38 +0530

    Sales table with full record

--- NewApp.py
+++ NewApp.py
@@ -1,30 +1,30 @@
-from sqlalchemy import create_engine
-from cubes.tutorial.sql import create_table_from_csv
+# from sqlalchemy import create_engine
+# from cubes.tutorial.sql import create_table_from_csv
 
-engine = create_engine('sqlite:///data.sqlite')
-
-create_table_from_csv(engine,
-                      "IBRD_Balance_Sheet__FY2010.csv",
-                      table_name="ibrd_balance",
-                      fields=[
-                            ("category", "string"),
-                            ("category_label", "string"),
-                            ("subcategory", "string"),
-                            ("subcategory_label", "string"),
-                            ("line_item", "string"),
-                            ("year", "integer"),
-                            ("amount", "integer")],
-                      create_id=True
-                  )
+# engine = create_engine('sqlite:///data.sqlite')
+#
+# create_table_from_csv(engine,
+#                       "IBRD_Balance_Sheet__FY2010.csv",
+#                       table_name="ibrd_balance",
+#                       fields=[
+#                             ("category", "string"),
+#                             ("category_label", "string"),
+#                             ("subcategory", "string"),
+#                             ("subcategory_label", "string"),
+#                             ("line_item", "string"),
+#                             ("year", "integer"),
+#                             ("amount", "integer")],
+#                       create_id=True
+#                   )
 
 
 from cubes import Workspace
 
 workspace = Workspace()
 workspace.register_default_store("sql", url="postgresql://postgres:N#123456@localhost/willowood")
-workspace.import_model("Sales_model.json")
+workspace.import_model("SalesTable.json")
 
-browser = workspace.browser("clv_sales_registerfcube")
+browser = workspace.browser("salestable")
 
 result = browser.aggregate()
 
@@ -33,7 +33,7 @@ print(result.summary["record_count"])
 print(result.summary["Qty"])
 print(result.summary["Value"])
 
-# result = browser.aggregate(drilldown=["fiscal_year"])
-#
-# for record in result:
-#     print(' record: ', record)
+result = browser.aggregate(drilldown=["billing_date"])
+
+for record in result:
+    print(' record: ', record)
